c7c6a40c9ff10ecaaeed66838e1e0ad4b4edbe01
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-06-07 16:16:57 +0300

    add example content

---
+++ CS/DXApplication7/DesignerForm1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WinDesignerDashboardState
+{
+    partial class DesignerForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.dashboardBarAndDockingController1 = new DevExpress.DashboardWin.Native.DashboardBarAndDockingController(this.components);
+            this.dashboardDesigner = new DevExpress.DashboardWin.DashboardDesigner();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardBarAndDockingController1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardDesigner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dashboardBarAndDockingController1
+            // 
+            this.dashboardBarAndDockingController1.PropertiesBar.AllowLinkLighting = false;
+            this.dashboardBarAndDockingController1.PropertiesDocking.ViewStyle = DevExpress.XtraBars.Docking2010.Views.DockingViewStyle.Classic;
+            // 
+            // dashboardDesigner
+            // 
+            this.dashboardDesigner.AllowMaximizeAnimation = true;
+            this.dashboardDesigner.AllowMaximizeDashboardItems = true;
+            this.dashboardDesigner.AllowPrintDashboard = true;
+            this.dashboardDesigner.AllowPrintDashboardItems = true;
+            this.dashboardDesigner.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.dashboardDesigner.Appearance.Options.UseBackColor = true;
+            this.dashboardDesigner.BarAndDockingController = this.dashboardBarAndDockingController1;
+            this.dashboardDesigner.DataSourceWizard.ShowConnectionsFromAppConfig = true;
+            this.dashboardDesigner.DataSourceWizard.SqlWizardSettings.DatabaseCredentialsSavingBehavior = DevExpress.DataAccess.Wizard.SensitiveInfoSavingBehavior.Prompt;
+            this.dashboardDesigner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardDesigner.Location = new System.Drawing.Point(0, 0);
+            this.dashboardDesigner.Name = "dashboardDesigner";
+            this.dashboardDesigner.Size = new System.Drawing.Size(1198, 599);
+            this.dashboardDesigner.TabIndex = 0;
+            this.dashboardDesigner.UseNeutralFilterMode = true;
+            // 
+            // DesignerForm1
+            // 
+            this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.False;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 599);
+            this.Controls.Add(this.dashboardDesigner);
+            this.Name = "DesignerForm1";
+            this.Text = "Dashboard Designer";
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardBarAndDockingController1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardDesigner)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardWin.DashboardDesigner dashboardDesigner;
+        private DevExpress.DashboardWin.Native.DashboardBarAndDockingController dashboardBarAndDockingController1;
+    }
+}
---
+++ CS/DXApplication7/DesignerForm1.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Xml.Linq;
+using DevExpress.DashboardCommon;
+
+namespace WinDesignerDashboardState
+{
+    public partial class DesignerForm1: DevExpress.XtraBars.Ribbon.RibbonForm {
+
+        DashboardState state = new DashboardState();
+        const string statePath = @"..\..\DashboardState\dashboardState.xml";
+        public DesignerForm1() {
+            InitializeComponent();
+            dashboardDesigner.CreateRibbon();
+            dashboardDesigner.LoadDashboard(@"..\..\Dashboards\dashboard1.xml");
+            if(File.Exists(statePath)) {
+                using(Stream stream = new FileStream(statePath, FileMode.Open)) {
+                    state.LoadFromXml(XDocument.Load(stream));
+                }
+            }
+            dashboardDesigner.SetDashboardState(state);
+            dashboardDesigner.DashboardSaved += DashboardDesigner_DashboardSaved;
+
+        }
+        protected override void OnClosing(CancelEventArgs e) {
+            base.OnClosing(e);
+            state = dashboardDesigner.GetDashboardState();
+            state.SaveToXml().Save(statePath);
+        }
+        private void DashboardDesigner_DashboardSaved(object sender,DevExpress.DashboardWin.DashboardSavedEventArgs e) {
+            state = dashboardDesigner.GetDashboardState();
+        }
+    }
+}
