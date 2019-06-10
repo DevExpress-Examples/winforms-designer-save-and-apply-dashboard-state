81d3fb2f8e7c786d7c0ec9bc916935bcc9aabbaa
DevExpressExampleBot <fakeEmail>
2019-06-10 18:30:53 +0300

    Source auto update [skip ci]

---
+++ VB/DXApplication7/DesignerForm1.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.Xml.Linq
+Imports DevExpress.DashboardCommon
+
+Namespace WinDesignerDashboardState
+	Partial Public Class DesignerForm1
+		Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+
+		Private dState As New DashboardState()
+		Private Const path As String = "..\..\Dashboards\dashboard1.xml"
+		Public Sub New()
+			InitializeComponent()
+			dashboardDesigner.CreateRibbon()
+			dashboardDesigner.LoadDashboard(path)
+		End Sub
+
+		Protected Overrides Sub OnClosing(ByVal e As CancelEventArgs)
+			MyBase.OnClosing(e)
+			dState = dashboardDesigner.GetDashboardState()
+			Dim userData As New XElement("Root", New XElement("DateModified",DateTime.Now), New XElement("DashboardState",dState.SaveToXml().ToString(SaveOptions.DisableFormatting)))
+			dashboardDesigner.Dashboard.UserData = userData
+			dashboardDesigner.Dashboard.SaveToXml(path)
+		End Sub
+		Private Sub dashboardDesigner_DashboardLoaded(ByVal sender As Object, ByVal e As DevExpress.DashboardWin.DashboardLoadedEventArgs) Handles dashboardDesigner.DashboardLoaded
+			Dim data As XElement = e.Dashboard.UserData
+			If data IsNot Nothing Then
+				If data.Element("DashboardState") IsNot Nothing Then
+					Dim dStateDocument As XDocument = XDocument.Parse(data.Element("DashboardState").Value)
+					dState.LoadFromXml(XDocument.Parse(data.Element("DashboardState").Value))
+				End If
+			End If
+		End Sub
+		Private Sub dashboardDesigner_SetInitialDashboardState(ByVal sender As Object, ByVal e As DevExpress.DashboardWin.SetInitialDashboardStateEventArgs) Handles dashboardDesigner.SetInitialDashboardState
+			e.InitialState = dState
+		End Sub
+	End Class
+End Namespace
